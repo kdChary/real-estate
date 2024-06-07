d97d9cf4e780d5883b008f6f65e4fb4eae05efc2
Siva Dinesh .k <ksd.ccbp4@gmail.com>
2024-06-07 20:32:59 +0530

    components and styling edited

--- estate-ui/src/App.js
+++ estate-ui/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+
 import './App.scss';
+import Home from './pages/homePage/Home';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
-	return <div>Now Start building your app</div>;
+	return (
+		<main className="layout">
+			<Navbar />
+			<section className="page">
+				<Home />
+			</section>
+		</main>
+	);
 };
 
 export default App;
